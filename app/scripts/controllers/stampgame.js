c18aaad48f82ae4ddb87593633d0f30a96ae54f7
Robb Lee <rlee@rallydev.com>
2014-05-21 10:31:29 -0600

    Enable basic dyanmic templating for games

--- app/scripts/controllers/stampgame.js
+++ app/scripts/controllers/stampgame.js
@@ -2,7 +2,7 @@
 
 angular.module('mathFactsApp').controller('StampgameCtrl', ['$scope',
   function($scope) {
-    $scope.gametemplate = 'views/stampgame.html';
+    $scope.gametemplate = 'stampgame';
     $scope.gameName = 'Stamp Game';
     $scope.message = "Let's Do Some Stamp Math!";
   }
